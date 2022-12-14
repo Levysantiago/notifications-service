dda877e92777d1d53094a468ca5cd7c97c9a5a43
Levy Santiago <levyssantiago@gmail.com>
2022-12-14 16:07:18 -0300

    feat: creating send notification service and repository

---
+++ src/app/use-cases/send-notification.ts
@@ -0,0 +1,34 @@
+import { Content } from '../entities/content';
+import { Notification } from '../entities/notification';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+
+interface ISendNotificationRequest {
+  recipientId: string;
+  content: string;
+  category: string;
+}
+
+interface ISendNotificationResponse {
+  notification: Notification;
+}
+
+export class SendNotification {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
+  async execute(
+    request: ISendNotificationRequest,
+  ): Promise<ISendNotificationResponse> {
+    const { recipientId, content, category } = request;
+
+    const notification = new Notification({
+      recipientId,
+      content: new Content(content),
+      category,
+    });
+
+    // Persist notification to database
+    this.notificationsRepository.create(notification);
+
+    return { notification };
+  }
+}
